f0a1789687eecfd8cb7f416ae470cbacc2e4b57d
Coach-BWise <wisebry11@gmail.com>
2020-03-25 21:24:36 -0400

    completed all functions and enhanced the README to reflect a finished project

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # lira-node-app
+
+This Project uses varius API calls with node to find info on movies, songs, and concerts. 
+
+## Getting Started
+
+Clone the repo. 
+
+### Prerequisites
+
+Be sure to run "npm i" to install the dependences. You will need to generate credentials for the Spotify API. See below steps.
+
+Step One: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
+
+Step Two: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
+
+Step Three: On the next screen, scroll down to where you see your client id and client secret. 
+
+Step Four: Create a .env file and copy and paste the below code replacing the values 'your_key_here' with your client id and client secret.
+
+```
+# Spotify API keys
+
+SPOTIFY_ID=your_key_here
+SPOTIFY_SECRET=your_key_here
+```
+
+You will also need an OMDB API Key. You can sign up for one [here.](http://www.omdbapi.com/apikey.aspx)
+
+
+Finally a Bands In Town API Key [here.](https://www.artists.bandsintown.com/login)
+
+## Running the tests
+
+Below are the commands able to be ran. 
+
+
+### node liri.js concert-this <artist/band name here>
+
+This inables the user to search for upcoming shows for any artist they choose. 
+
+See example: [concert](https://github.com/Coach-BWise/lira-node-app/blob/master/screenshots/concert-this.jpg)
+
+### node liri.js spotify-this-song '<song name here>'
+
+The user can get full details on a song of their choosing (results for "The Sign" are returned if no perameters are passed).
+
+See example: [spotify](https://github.com/Coach-BWise/lira-node-app/blob/master/screenshots/spotify-this-song.jpg)
+
+### node liri.js movie-this '<movie name here>'
+
+The user can get full details on a movie of their choosing (results for "Mr. Nobody" are returned if no perameters are passed)
+
+See example: [movie](https://github.com/Coach-BWise/lira-node-app/blob/master/screenshots/movie-this.jpg)
+
+### node liri.js do-what-it-says
+
+This will read the command of the random.txt file and run one of the above based on what is passed in this file. 
+
+See example: [do it](https://github.com/Coach-BWise/lira-node-app/blob/master/screenshots/do-what-it-says.jpg)
+
+## Built With
+
+* [Node](https://nodejs.org/en/)
+* [Axios](https://www.npmjs.com/package/axios)
+
+## Author
+
+**Bryan Wise** 
--- liri.js
+++ liri.js
@@ -1,59 +1,3 @@
-// //manifest
-// require("dotenv").config();
-// var axios = require("axios");
-// var keys = require("./keys.js");
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify(keys.spotify);
-// var fs = require("fs");
-// var moment = require(`moment`);
-// var inquirer = require("inquirer");
-
-// // vars for calls
-// var logger = fs.createWriteStream(`log.txt`,{flags:`a`});
-// var artist = ""
-// var movieQ = 'http://www.omdbapi.com/?i=tt3896198&apikey=65fae755'
-// var concertQ = `"https://rest.bandsintown.com/artists/" + ${artist} + "/events?app_id=codingbootcamp"`
-
-// // create timestamp
-// let timestamp = moment().format(`MM/DD/YYYY h:mm:ss a`)
-
-// // command line args
-// var variable =process.argv[3]
-// var statement=process.argv[2]
-
-// // Create a variable to handle the full search argument
-// let argument = "";
-// getArgument();
-
-// function getArgument(){
-
-//     // grab the command line arguments
-//     let args = process.argv;
-//     // loop through the arguments starting at the 3rd index
-//     for (let i=3; i<args.length; i++) {
-//         if (i>3 && i<args.length) {argument = argument + " " + args[i]
-//     } else argument += args[i];
-//     }
-// }
-
-// switch (statement) {
-//     case "concert-this":
-//       concertThis(variable);
-//       break;
-    
-//     case "spotify-this-song":
-//       spotifySong(variable);
-//       break;
-    
-//     case "movie-this":
-//       movieThis(variable);
-//       break;
-    
-//     case "do-what-it-says":
-//       doWhat(variable);
-//       break;
-//     }
-
 require("dotenv").config();
 
 const keys = require("./keys");
@@ -90,7 +34,7 @@ switch (statement) {
       break;
     
     case "spotify-this-song":
-      spotifySong();
+      spotifySong(argument);
       break;
     
     case "movie-this":
@@ -136,6 +80,8 @@ for (let i=0; i < response.data.length; i++) {
     let date = moment(response.data[i].datetime).format(`MM/DD/YYYY`);
     console.log(`Result #${i+1}`);
     console.log(`City: ${city}, ${region}, at ${venue} on ${date}\n`);
+    logger.write(`Result #${i+1}\n`);
+    logger.write(`City: ${city}, ${region}, at ${venue} on ${date}\n`)
 }}) 
 
   .catch(function(error) {
@@ -158,8 +104,6 @@ for (let i=0; i < response.data.length; i++) {
       }
       console.log(error.config);
     }
-  
-
   )}
 
 //spotify-this-song
@@ -168,8 +112,8 @@ function spotifySong(){
     console.log(argument);
      if (argument=="") { 
          argument = "The Sign"
-    } else{  
-        // Then run a request with axios to the Bands in Town API with the artist specified
+      }
+        // Then run a request with axios to the spotify API with the song specified
         spotify.search({type: 'track', query: argument}, (function(err, response){
             if (err){
                 return console.log('Error occurred: ' + err);
@@ -177,9 +121,10 @@ function spotifySong(){
                 let items = response.tracks.items;
 
                 for (var i=0; i< items.length; i++){
+                  
                     let artists = "";
-                    for (var j =0; j<items[i].artists[j].length; j++){
-                        if (j==parseInt(items[i].artists[j].length) - 1){
+                    for (var j =0; j<items[i].artists.length; j++){
+                        if (j==parseInt(items[i].artists.length) - 1){
                         artists += items[i].artists[j].name;
                         } else {
                             artists += `${items[i].artists[j].name} and `;
@@ -187,96 +132,94 @@ function spotifySong(){
                     }
                     let song=items[i].name;
                     let preview = "";
-                    if (items[i].preview_Url == null) { 
+                    if (items[i].preview_url == null) { 
                         preview ="No preview link available"
                     } else {
-                        preview = items[i].preview_Url;
+                        preview = items[i].preview_url;
                     }
                     let album = items[i].album.name
                     console.log(`------------------------\nResult #${i+1}`)
                     console.log(`Song Name: ` + song);
-                    console.log(`artist(s): `+ typeof(artists) + "\n");
+                    console.log(`artist(s): `+ artists);
+                    console.log(`Album: ` + album);
                     console.log(`Preview Song: ` + preview);
-                    console.log(`Album: ` + album)
-                    logger.write(`-----------------------` + "\n");
-                    logger.write(i + "\n");
-                    logger.write(`Artist(s)`+ artists + "\n");
+                    logger.write(`-----------------------\nResult #${i+1}`);
                     logger.write("Song name: " + song + `\n`);
-                    logger.write(`Preview Song: ` + preview + "\n");
+                    logger.write(`Artist(s)`+ artists + "\n");
                     logger.write('Album: ' + album)
-                }
+                    logger.write(`Preview Song: ` + preview + "\n");
+                };
             }
         }))
     }
-}
 //  movie-this
     function movieThis(argument){
-        if (argument="") { argument = "Mr. Nobody" }
-        else{
-            axios.get("http://www.omdbapi.com/?t=" + argument + "&apikey=trilogy").then(
-                function(response) {
-                  console.log("Title: " + response.data.title);
-                  console.log("Year: " + response.data.Year);
-                  console.log("The movie's rating is: " + response.data.imdbRating);
-                  console.log("The movie's rating is: "+ response.data.Ratings[1].Value);
-                  console.log("Country: " + response.data.Country);
-                  console.log("Language: " + response.data.Language);
-                  console.log("Plot: " + response.data.Plot);
-                  console.log("Actors: " + response.data.Actors);
-                  logger.write(`------------------------------`);
-                  logger.write("Title: " + response.data.title);
-                  logger.write("Year: " + response.data.Year);
-                  logger.write("The movie's rating is: " + response.data.imdbRating);
-                  logger.write("The movie's rating is: "+ response.data.Ratings[1].Value);
-                  logger.write("Country: " + response.data.Country);
-                  logger.write("Language: " + response.data.Language);
-                  logger.write("Plot: " + response.data.Plot); 
-                  logger.write("Actors: " + response.data.Actors)
-                })
-          .catch(function(error) {if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-          }
-      
-    
-   , 
-)}
-        // do-what-it-says
-        function doWhat(){
-             // This block of code will read from the "random.txt" file.
+      if (argument=="") { 
+        argument = "Mr. Nobody" 
+      };
+      axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${argument}`)
+        
+      .then(
+            function(response) {
+              console.log("Title: " + response.data.Title);
+              console.log("Year: " + response.data.Year);
+              console.log("The movie's rating is: " + response.data.imdbRating);
+              console.log("Country: " + response.data.Country);
+              console.log("Language: " + response.data.Language);
+              console.log("Plot: " + response.data.Plot);
+              console.log("Actors: " + response.data.Actors);
+              logger.write(`------------------------------`+ "\n");
+              logger.write("Title: " + response.data.title+ "\n");
+              logger.write("Year: " + response.data.Year+ "\n");
+              logger.write("The movie's rating is: " + response.data.imdbRating+ "\n");
+              logger.write("Country: " + response.data.Country+ "\n");
+              logger.write("Language: " + response.data.Language+ "\n");
+              logger.write("Plot: " + response.data.Plot+ "\n"); 
+              logger.write("Actors: " + response.data.Actors+ "\n")    
+      })
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      })         
+    }  
+    // do-what-it-says
+    function doWhat(){
+    // This block of code will read from the "random.txt" file.
     // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
     // The code will store the contents of the reading inside the variable "data"
-    fs.readFile("random.txt", "utf8", function (error, data) {
+      fs.readFile("random.txt", "utf8", function (error, data) {
 
         // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
-        }
+        } else{
 
-        // Then split it by commas (to make it more readable)
-        var dataArr = data.split(",");
+            // Then split it by commas (to make it more readable)
+            var dataArr = data.split(",");
+            
+            let action = dataArr[0];
+            argument = dataArr[1];
 
-        let action = dataArr[0];
-        let query = dataArr[1];
-
-        if (action = "concert-this") {concertThis(query)}
-        else if (action = "spotify-this-song") {spotifySong(query)}
-        else if (action = "movie-this") {movieThis(query)}
-        else {console.log("I don't know what to do????????")}
-
-            })}}
+            if (action == "concert-this") {concertThis(argument);}
+            else if (action == "spotify-this-song") {spotifySong(argument);}
+            else if (action == "movie-this") {movieThis(argument);}
+            else {console.log("I don't know what to do????????")}
+          }
+      })
+    }
